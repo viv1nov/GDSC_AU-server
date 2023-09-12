1fb61f91cc01da62c497daab8a539af9df3fd164
Vivek <122638553+vivekbopaliya@users.noreply.github.com>
2023-09-12 17:39:07 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,42 +7,14 @@ import {
   updatePoints,
   updateUser,
 } from "../controllers/users.js";
-import multer from "multer";
-import { v4 as uuidv4 } from "uuid";
 
-import path from "path";
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, uuidv4() + "-" + Date.now() + path.extname(file.originalname));
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  const allowedFileTypes = ["image/jpeg", "image/jpg", "image/png"];
-  if (allowedFileTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-const upload = multer({
-  storage,
-  fileFilter,
-  limits: {
-    fileSize: 10 * 1024 * 1024,
-  },
-});
 
 router.get("/currentUser/:userID", getCurrentUser);
 router.get("/singleUser/:resID", singleUser);
 router.get("/getAllUsers", getAllUsers);
-router.patch("/updateUser/:userID", upload.single("file"), updateUser);
+router.patch("/updateUser/:userID",  updateUser);
 router.put("/updatePoints/:resID", updatePoints);
 
 router.delete("/deleteUser/:userID", deleteUser);
